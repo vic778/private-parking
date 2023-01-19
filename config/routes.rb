b17b23ede21ef168d019c854a884b207fc587b48
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-19 16:33:35 +0400

    Create route for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :admin do
+      resources :roles
+      resources :users
+
+      root to: "roles#index"
+    end
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users
 end
