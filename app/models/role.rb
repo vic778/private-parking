2f3daae820e10ba88576e4ae8fd756bcfcc497f5
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-19 22:16:01 +0400

    Creta the permission constance

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,11 +1,43 @@
 class Role < ApplicationRecord
   has_many :users
-  # has_many :customers, class_name: 'User', foreign_key: 'user_id'
 
-  enum role: {
+  PERMISSIONS = {
+    'admin' => {
+      all: {
+        index: true,
+        show: true,
+        create: true,
+        update: true,
+        destroy: true
+      }
+    },
+    'user' => {
+      parking: {
+        index: true,
+        show: true,
+        create: true,
+        update: true,
+        destroy: true
+      },
+      slot: {
+        index: true,
+        show: true,
+        create: true,
+        update: true,
+        destroy: true
+      }
+    }
+  }.freeze
+
+  enum name: {
     user: 0,
     admin: 1
   }
+
+  def has_permission?(action, resource)
+    PERMISSIONS.dig(name, resource, action) ||
+      PERMISSIONS.dig(name, :all, action)
+  end
 
   validates :name, presence: true, uniqueness: true
 end
