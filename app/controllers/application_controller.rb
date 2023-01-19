8500b2e42e1c5349bdfe2342f2db3f6223825b32
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-19 16:54:36 +0400

    Include pundit to rescue the autjorization error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    include Pundit::Authorization
+    include ApplicationHelper
+  
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+    private
+  
+    def user_not_authorized
+      flash[:alert] = t("flash.pundit.unauthorized")
+      redirect_back(fallback_location: root_path)
+    end
 end
