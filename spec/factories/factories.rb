504d199dddea4a374efa2502969ccd34a7355162
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-20 12:25:55 +0400

    Generate all models fir factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,25 +1,48 @@
 FactoryBot.define do
-  # factory :role do
-  #   name { Role.names.keys.sample }
+  factory :role do
+    name { Role.names.keys.sample }
 
-  #   to_create do |instance|
-  #     instance.id = Role.find_or_create_by(name: instance.name).id
-  #     instance.reload
-  #   end
-  # end
+    to_create do |instance|
+      instance.id = Role.find_or_create_by(name: instance.name).id
+      instance.reload
+    end
+  end
 
-  # factory :user do
-  #   sequence :email do |n|
-  #     "person#{n}@example.com"
-  #   end
-  #   name { "John Doe" }
+  factory :user do
+    sequence :email do |n|
+      "person#{n}@example.com"
+    end
+    name { "John Doe" }
 
-  #   password { "password" }
+    password { "password" }
 
-  #   association :role, factory: :role, name: 'user'
+    association :role, factory: :role, name: 'user'
 
-  #   trait :admin do
-  #     association :role, factory: :role, name: 'admin'
-  #   end
-  # end
+    trait :admin do
+      association :role, factory: :role, name: 'admin'
+    end
+  end
+
+  factory :parking do
+    name { "MyString" }
+    address { "MyString" }
+    open_time { "2023-01-20 10:17:56" }
+    close_time { "MyString" }
+  end
+
+  factory :slot_type do
+    name { "MyString" }
+
+    association :parking, factory: :parking
+  end
+
+  factory :slot do
+    name { "MyString" }
+    price { "MyString" }
+    open_time { "2023-01-20 11:26:48" }
+    close_time { "2023-01-20 11:26:48" }
+    status { 1 }
+
+    association :slot_type, factory: :slot_type
+  end
 end
