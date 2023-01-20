d61760afb8ce87201eb70def0a1de7e2ff23ad4c
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-20 12:19:27 +0400

    Genrate the slot controller nd models in the admin namespace, associate it ot the stole_type

---
+++ app/dashboards/slot_dashboard.rb
@@ -0,0 +1,81 @@
+require "administrate/base_dashboard"
+
+class SlotDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    name: Field::String,
+    price: Field::Number.with_options(decimals: 2),
+    open_time: Field::DateTime,
+    close_time: Field::DateTime,
+    status: Field::Select.with_options(searchable: false, collection: ->(field) { field.resource.class.send(field.attribute.to_s.pluralize).keys }),
+    slot_type: Field::BelongsTo,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    name
+    price
+    slot_type
+    status
+    close_time
+    open_time
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    name
+    price
+    open_time
+    close_time
+    status
+    slot_type
+    created_at
+    updated_at
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    name
+    price
+    open_time
+    close_time
+    status
+    slot_type
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how slots are displayed
+  # across all pages of the admin dashboard.
+  #
+  def display_resource(slot)
+    slot.name
+  end
+end
