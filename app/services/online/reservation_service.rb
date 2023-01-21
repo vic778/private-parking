fa662ff3b831721d2a8315d22a0b205768cadb05
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-21 23:54:54 +0400

    Check if the reservation_timestamp is greater than given time and charge the ustomer

--- app/services/online/reservation_service.rb
+++ app/services/online/reservation_service.rb
@@ -1,6 +1,6 @@
 module Online
   class ReservationService
-    attr_accessor :customer, :slot, :params
+    attr_accessor :customer, :slot
 
     def initialize(customer, slot)
       @customer = customer
@@ -25,6 +25,37 @@ module Online
       else
         error(reservation.errors.full_messages.join(", "))
       end
+    end
+
+    def cancel_reservation(reservation)
+      now = Time.zone.now
+      time_passed = (Time.zone.now - reservation.created_at) / 60
+      # binding.pry
+      if time_passed > 1800 # 1800 seconds = 30 min
+        error("Reservation can not be canceled because it was created more than 30 minutes ago")
+      elsif time_passed > 300 # 300 seconds = 5 min
+        cancel_without_charge(reservation)
+      elsif time_passed < 900 # 900 seconds = 15 min
+        cancel_with_charge(reservation, 0.02)
+      elsif time_passed < 1500 # 1500 seconds = 25 min
+        cancel_with_charge(reservation, 0.1)
+      else
+        error("Reservation can not be canceled because it was created more than 25 minutes ago")
+      end
+    end
+
+    def cancel_without_charge(reservation)
+      reservation.update(cancelled_at: Time.now, status: "cancelled")
+      slot.update(status: "available")
+      success(reservation)
+    end
+
+    def cancel_with_charge(reservation, charge_percentage)
+      # binding.pry
+      charge = (reservation.total_price.to_f * charge_percentage).round(2)
+      reservation.update(cancelled_at: Time.zone.now, status: "cancelled", cancellation_charge: charge)
+      slot.update(status: "available")
+      success(reservation)
     end
 
     def success(reservation)
