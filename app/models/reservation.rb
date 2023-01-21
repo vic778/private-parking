4f1ad96a070612f5563cb98d17709a65c6e86860
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-21 16:13:12 +0400

    Comment the format to show the format

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,17 +2,15 @@ class Reservation < ApplicationRecord
   belongs_to :slot
   belongs_to :customer, class_name: 'User', foreign_key: 'user_id'
 
-  validates :from, :to, presence: true
+  validates :from, :to, presence: true # format: { with: /\A\d{4}-\d{2}-\d{2}T\d{2}:\d{2}\z/, message: 'must be in the format YYYY-MM-DDTHH:MM' }
   validates :number_of_hours, numericality: { greater_than: 0 }, if: :number_of_hours?
   validate :from_is_before_to, :from_is_in_future
-  # validates :slot_type, presence: true
-
-  # def total_price
-  #   self.total_price * self.number_of_hours
-  # end
+  validates :slot_type, presence: true
 
   def from_is_before_to
     return if from.blank? || to.blank?
+
+    # binding.pry
 
     errors.add(:to, 'must be greather than from') if to <= from
   end
