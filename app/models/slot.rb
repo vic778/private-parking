5d266147b123717586a500af80d2ea646e32166f
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-21 13:49:48 +0400

    Delete the method to convert the hours

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -3,8 +3,6 @@ class Slot < ApplicationRecord
   has_one :reservation
   has_one :booking
 
-  validate :validate_slot_status
-
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
   validates :open_time, presence: true
@@ -16,12 +14,5 @@ class Slot < ApplicationRecord
     booked: 1,
     closed: 2
   }
-
-  # def update_slot_status
-  #   if self.reservation.present?
-  #     self.status = 1
-  #   else
-  #     self.status = 0
-  #   end
-  # end
+  
 end
