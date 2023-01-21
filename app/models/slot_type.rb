e31d5322ab73e8b1e7a156ef0011d06ad2749d8e
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-21 13:51:04 +0400

    Make the stolt_type_opntion in camelcase

--- app/models/slot_type.rb
+++ app/models/slot_type.rb
@@ -1,7 +1,7 @@
 class SlotType < ApplicationRecord
   belongs_to :parking
   has_many :slots
-  SLOTTYPES_OPTIONS = %w[Motorcycle Car Truck per desabled_people].freeze
+  SLOTTYPES_OPTIONS = %w[motorcycle car truck desabled_people].freeze
 
   validates :name, inclusion: { in: SLOTTYPES_OPTIONS }, presence: true, uniqueness: true
 end
