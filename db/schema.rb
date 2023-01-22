8db25f8386d5773786305036af40a6c3398e600b
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-22 16:27:41 +0400

    Add by mifration the status to booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_160402) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_22_120950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_160402) do
     t.bigint "slot_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
     t.index ["slot_id"], name: "index_bookings_on_slot_id"
   end
 
