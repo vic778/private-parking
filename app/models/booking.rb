196cd26f9fee7afcbd4fb95aa9ab923f4739dbcd
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-22 16:26:37 +0400

    Create method to assign the current_time to arrival_time after create

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :slot
   before_create :set_arrival_time, :set_left_time_to_nil
 
+  validates :license_plate, presence: true, length: { minimum: 6, maximum: 8 }
+
   private
 
   def set_arrival_time
