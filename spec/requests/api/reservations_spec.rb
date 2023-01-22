bee70aa1cf715c0d02df5133454023edddf7d632
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-23 00:19:06 +0400

    Login a user to perfrom actions

--- spec/requests/api/reservations_spec.rb
+++ spec/requests/api/reservations_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
+require './spec/support/sign_in'
 
 RSpec.describe "Api::Reservations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "when as a customer" do
+    before(:each) do
+      @user = sign_in_customer
+      @headers = {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: "Bearer #{@user['token']}",
+        uid: @user['email']
+      }
+    end
+
+    # describe "Get all the reservations for the customer" do
+    #   it "returns all the reservations" do
+    #     reservation = create(:reservation, customer: @user)
+    #     get '/api/reservations', headers: @headers
+    #     expect(response.body).to eq([reservation].to_json)
+    #   end
+    # end
   end
 end
